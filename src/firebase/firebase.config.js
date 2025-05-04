1b4ecb47297d63ad11608b9a2a75d276b555f24b
md-zeon <zeonrahaman5870@gmail.com>
2025-05-04 09:29:24 +0600

    Added User Context and Provider

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyDXDEjf2tNANiX0A9s0S2ZdJ4Hrh_pStFg",
+	authDomain: "dragon-news-e638f.firebaseapp.com",
+	projectId: "dragon-news-e638f",
+	storageBucket: "dragon-news-e638f.firebasestorage.app",
+	messagingSenderId: "153689570879",
+	appId: "1:153689570879:web:1c01d41b23589a6d6a828a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
